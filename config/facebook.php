3c499abaa6b1526fca9912b61c4f4e67aaad2301
Mike Smullin <mike@smullindesign.com>
2011-08-31 19:51:22 -0600

    reconfig fb fork

--- config/facebook.php
+++ config/facebook.php
@@ -1,34 +1,34 @@
 <?php
 
-switch (Configure::read('YourApp.environment')) {
+switch (Configure::write('Qoggo.environment', $env)) {
 	default:
 	case 'production':
 		$config['Facebook'] = array(
-		  'APP_ID'        => '123415513359764',
-		  'APP_URL'       => '',
-		  'API_KEY'       => '256172113356114',
-		  'API_SECRET'    => '1231289a3fcbaf43a4624953bfa9950f',
-		  'COOKIE_DOMAIN' => 'www.yourapp.com'
+			'APP_ID'		=> '266575813359764',
+			'APP_URL'		=> '',
+			'API_KEY'		=> '266575813359764',
+			'API_SECRET'	=> '8eaf0a541d2baf43a4624953bfa9950f',
+			'COOKIE_DOMAIN' => 'www.qoggo.com'
 		);
 		break;
 
 	case 'staging':
 		$config['Facebook'] = array(
-		  'APP_ID'        => '161241511712556',
-		  'APP_URL'       => '',
-		  'API_KEY'       => '152343245166551',
-		  'API_SECRET'    => '110dcdcac33aga37788acf3cb2284d2b',
-		  'COOKIE_DOMAIN' => 'dev.yourapp.com'
+			'APP_ID'		=> '150331101712556',
+			'APP_URL'		=> '',
+			'API_KEY'		=> '150331101712556',
+			'API_SECRET'	=> '660dcdcde701da77788acf3cb2284d2b',
+			'COOKIE_DOMAIN' => 'dev.qoggo.com'
 		);
 		break;
 
-	case 'local':
+	case 'local': // mike's local
 		$config['Facebook'] = array(
-		  'APP_ID'        => '247817041971157',
-		  'APP_URL'       => 'https://apps.facebook.com/yourapp_local/',
-		  'API_KEY'       => '242341243516757',
-		  'API_SECRET'    => '4d567af567asdf75a99a8d76657b0169',
-		  'COOKIE_DOMAIN' => 'yourapp.local'
+			'APP_ID'		=> '247165811976757',
+			'APP_URL'		=> 'https://apps.facebook.com/qoggo_local/',
+			'API_KEY'		=> '247165811976757',
+			'API_SECRET'	=> '5f8e9314373816419c783cef657b0169',
+			'COOKIE_DOMAIN' => 'qoggo.smullindesign.com'
 		);
 		break;
 }
